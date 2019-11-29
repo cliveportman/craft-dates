4ce6c297f1944aae3858df5cae90e422c5a07d18
cliveportman <clive@cliveportman.co.uk>
2019-11-29 09:58:06 +0000

    v1.0.4

--- src/Dates.php
+++ src/Dates.php
@@ -45,7 +45,7 @@ class Dates extends Plugin
     /**
      * @var string
      */
-    public $schemaVersion = '1.0.2';
+    public $schemaVersion = '1.0.4';
 
     // Public Methods
     // =========================================================================
